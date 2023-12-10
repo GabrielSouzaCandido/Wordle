bf96baadb1a6215754885eecf1b572b92a67a693
Gabriel Souza <57289760+gabrielGaates@users.noreply.github.com>
2023-12-09 21:20:34 -0300

    Implementação da lógica básica de vitória ao fim da escrita

--- wordle.client/src/components/Word/Word.jsx
+++ wordle.client/src/components/Word/Word.jsx
@@ -6,40 +6,63 @@ function Word(onTextInput) {
     const [word, setWord] = useState([Array(6).fill("_")]);
     const [wordClass, setClass] = useState([Array(6).fill("Spotlight")]);
     const [currentMove, setCurrentMove] = useState(0);
+    let misteryWord = "Cavalo";
 
     const handleKeyPress = (event) => {
 
-    
+        
+     
         const nextWord = [...word];
         const nextClass = [...wordClass];
-        let nextMove = currentMove + 1;
+        let nextMove = currentMove;
         let newClass = "noClass";
-        if (nextMove > word[0].length - 1) nextMove = 0;
-        nextWord[0][currentMove] = event.key;
-        console.log(nextClass);
-        nextClass[0][currentMove] = newClass;
-        console.log(nextClass);
 
-        setCurrentMove(nextMove);
-        setWord(nextWord);
-        setClass(nextClass);
+        if (/^[a-zA-Z]$/.test(event.key) || event.key === 'Backspace') {
+            if (event.key === 'Backspace' && (currentMove > 0 || nextWord[0][5] != "-")) {
+
+                if (nextMove == 0 && nextWord[0][5] != "-") nextMove = word[0].length - 1;
+                nextWord[0][nextMove - 1] = '_';
+                nextClass[0][nextMove - 1] = newClass;
+
+                console.log(nextWord[0][5]);
+                setCurrentMove(nextMove - 1);
+                setWord(nextWord);
+                setClass(nextClass);
+            } else if (event.key != 'Backpace' && nextMove < word[0].length) {
+                let nextMove = currentMove + 1;
+                /*     if (nextMove > word[0].length - 1) nextMove = 0;*/
+                nextWord[0][currentMove] = event.key;
+                nextClass[0][currentMove] = newClass;
+
+                console.log(nextClass);               
+                console.log(nextClass);
+
+
+                setCurrentMove(nextMove);
+                setWord(nextWord);
+                setClass(nextClass);
+            }
+        }
+       
+        if (misteryWord == word[0].join().replace(/,/g, '')) {
+            alert('acertou');
+        }
+
+
 
 
     };
-    const ilustrate = word.map((character, index) => {       
-        return (
-            <li key={index} tabIndex={index} onKeyDown={handleKeyPress} className={wordClass[0][index]}>
-                <span key={index}>{word[index]}</span>
-            </li>
-        )
+ 
 
-    });
+    const ilustrate = word.map((character, index) => (
+        <span key={index} tabIndex={index} onKeyDown={handleKeyPress} className="Spotlight">
+            {word[index]}
+        </span>
+    ));
     return (
         <div>
-
             <p>{ilustrate}</p>
         </div>
     );
-
 }
 export default Word;
